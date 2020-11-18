fb2c3230c404cf5d654be09a801682ce275abd1e
Mia <mia@vidops.io>
2020-11-18 15:52:10 +0000

    tried adding parse , still working on it with the new json earthquakes and added some bits to main

--- Desktop/bitcoin-app/app/Main.hs
+++ Desktop/bitcoin-app/app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
 import HTTP
+import Parse
 
 main :: IO ()
 main = do
     let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
     json <- download url
-    print json
+    case (parse json) of 
+        Left err -> print err
+        Right feats -> print . head $ (features feats)
--- Desktop/bitcoin-app/src/Parse.hs
+++ Desktop/bitcoin-app/src/Parse.hs
@@ -2,8 +2,29 @@
 
 module Parse where
 
-import Data.Aeson
+import Data.Aeson (eitherDecode)
 import qualified Data.ByteString.Lazy.Char8 as L8
-import GHC.Generics
+import GHC.Generics ( Generic )
+import Data.Aeson.Types
+
+-- list of features in each earthquake
+data Features = Features {
+           features :: [Properties]
+    } deriving (Show, Generic)
+
+instance FromJSON Features
+instance ToJSON Features
+
+-- array of properties of earthquake 
+data Properties = Properties {
+           mag :: Double,
+           place :: String
+    } deriving (Show, Generic)
+
+instance FromJSON Properties
+instance ToJSON Properties
+
+parse :: L8.ByteString -> Either String Features
+parse json = eitherDecode json :: Either String Features 
 
 
