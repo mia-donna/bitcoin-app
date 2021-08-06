22bf7c5c3b5354b47b087280d9be55b8e9187282
Mia Allen <mia@vidops.io>
2021-08-06 23:54:55 +0100

    started homepage

---
+++ social-media-react-app/src/components/topbar/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import "./topbar.css";
+import { Search, Person } from "@material-ui/icons";
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+              <span classname="logo"> Lamasocial</span>
+            </div>
+            <div className="topbarCenter">
+              <div className="searchbar">
+                  <Search />
+                  <input placeholder="Serach for a friend, post or video" className="searchInput"/>
+              </div>
+            </div> 
+            <div className="topbarRight">
+               <div className="topbarLinks">
+                   <span classname="topbarLink"> Lamasocial</span>
+                   <span classname="topbarLink"> Timeline</span>
+               </div>
+               <div className="topbarIcons">
+                 <div className="topbarIconItem">
+                  <Person />
+                  <span className="topbarIconBage">1</span>
+                </div>
+               </div>
+            </div>
+        </div>
+    );
+}
+
+
