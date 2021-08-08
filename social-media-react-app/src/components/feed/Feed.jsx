65c669142e0086e0b67240336091cd526085b954
Mia Allen <mia@vidops.io>
2021-08-08 17:37:12 +0100

    add posts from json file dynamically

--- social-media-react-app/src/components/feed/Feed.jsx
+++ social-media-react-app/src/components/feed/Feed.jsx
@@ -1,6 +1,7 @@
 import "./feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
+import {Posts} from "../../dummyData";
 
 
 export default function Feed() {
@@ -8,12 +9,11 @@ export default function Feed() {
         <div className="feed">
             <div className="feedWrapper">
                 <Share/>
-                <Post />
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                {Posts.map((p) => (
+                 <Post key={p.id} post ={p}/>
+                ))}
+                
+                
 
             </div>
         </div>
